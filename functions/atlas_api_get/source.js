10906c31aa8fd49c3443b7c471cb10b507e32302
JamesOsgood <james.osgood@gmail.com>
2020-01-24 23:59:49 +0000

    Add functions to call atlas api

---
+++ functions/atlas_api_get/source.js
@@ -0,0 +1,35 @@
+ 
+exports = function(url) 
+{
+  const http = context.services.get('http');
+  
+  const mongodb = context.services.get("MasterAtlas");
+  
+  return http
+    .get({ url: url})
+    .then(resp => {
+      const username = context.values.get('AtlasPublicKey');
+      const apiKey = context.values.get('AtlasPrivateKey');
+      const authHeader = resp.headers['Www-Authenticate'].toString();
+      const realm = authHeader.match(/realm="(.*?)"/)[1];
+      const nonce = authHeader.match(/nonce="(.*?)"/)[1];
+      const qop = authHeader.match(/qop="(.*?)"/)[1];
+
+      const ha1 = utils.crypto.hash('md5', `${username}:${realm}:${apiKey}`).toHex();
+
+      const path = url.match(/:\/\/.*?(\/.*)/)[1];
+
+      const ha2 = utils.crypto.hash('md5', `GET:${path}`).toHex();
+      const cnonce = Math.random().toString().substr(2, 14);
+      const nc = '00000001';
+  
+      const response = utils.crypto.hash('md5', `${ha1}:${nonce}:${nc}:${cnonce}:${qop}:${ha2}`).toHex();
+
+      const digestHeader = `Digest username="${username}", realm="${realm}", nonce="${nonce}", uri="${path}", qop=${qop}, nc=${nc}, cnonce="${cnonce}", response="${response}", algorithm=MD5`;
+
+      return http.get({ url: url, headers: { 'Authorization': [ digestHeader ], "Content-Type": [ "application/json" ] } })
+            .then(({ body }) => {
+                return JSON.parse(body.text());
+              });
+    });
+};
