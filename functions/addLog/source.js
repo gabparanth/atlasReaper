6e5431e120ee376a4a341e085b9bda011fbaf47f
JamesOsgood <james.osgood@gmail.com>
2020-01-24 23:07:34 +0000

    Update add log

--- functions/addLog/source.js
+++ functions/addLog/source.js
@@ -1,14 +1,16 @@
 // type : error or success
 
-exports = function(type, message){
-
+exports = function(level, context, message, tag)
+{
   const mongodb = context.services.get("MasterAtlas");
-  const logCollection = mongodb.db("atlas").collection("log");
+  const logCollection = mongodb.db("atlas_reaper").collection("log");
   
   var date = new Date(Date.now());
   
-  var logObject = {"date": date, "type":type, "message": message};
-  
-  
+  var logObject = { "level" : level,
+                    "date": date, 
+                    "context" : context,
+                    "message": message,
+                    "tag" : tag };
   return logCollection.insertOne(logObject);
 };
