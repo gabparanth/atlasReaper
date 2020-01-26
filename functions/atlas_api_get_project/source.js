1dcfb9ace45a7ecafe5483dd9a671a14ca22eb95
JamesOsgood <james.osgood@gmail.com>
2020-01-26 20:50:53 +0000

    Get project

---
+++ functions/atlas_api_get_project/source.js
@@ -0,0 +1,7 @@
+ 
+exports = function(projectId, clusterName) 
+{
+    var url = "https://cloud.mongodb.com/api/atlas/v1.0/groups/"+projectId+"/clusters/" + clusterName;
+
+    return context.functions.execute("atlas_api_get", url);
+};
