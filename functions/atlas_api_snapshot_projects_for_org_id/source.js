313b560b12938b6ca44a70f0dcc88b0fae99e39c
JamesOsgood <james.osgood@gmail.com>
2020-01-26 23:31:39 +0000

    Snaphot clusters

--- functions/atlas_api_snapshot_projects_for_org_id/source.js
+++ functions/atlas_api_snapshot_projects_for_org_id/source.js
@@ -10,26 +10,27 @@ exports = function(org_id)
     clusterSnapshotsCollection.insertOne(snapshot).then(result => {
 
       const snapshot_id = result.insertedId;
-      context.functions.execute("atlas_api_get_projects_for_org_id", org_id).then(projects => {
+      context.functions.execute("atlas_api_get_projects_for_org_id", org_id).then(resp => {
+        var projects = resp.results;
+        projects.forEach(project => { 
+          context.functions.execute("atlas_api_get_clusters_for_project_id", project.id).then(resp => {
+            var clusters = resp.results;
+            clusters.forEach(cluster => {
+              
+              var clusterDoc = {
+                          "cluster_id": cluster.id,
+                          "name" : cluster.name,
+                          "project":{ "id": project.id,
+                          "name": project.name },
+                          "configuration" : cluster};
 
-      projects.forEach(project => { 
-        context.functions.execute("atlas_api_get_clusters_for_project_id", project.id).then(clusters => {
-          clusters.forEach(cluster => {
-            
-            var clusterDoc = {
-                        "cluster_id": cluster.id,
-                        "name" : cluster.name,
-                        "project":{ "id": project.id,
-                        "name": project.name },
-                        "configuration" : cluster};
-
-            clusterSnapshotsCollection.updateOne({"_id": snapshot_id}, { $push : { 'clusters' : clusterDoc } }).then(result => {
-              const { matchedCount, modifiedCount } = result;
-              if( matchedCount && modifiedCount ) {
-                context.functions.execute('log_message', 'INFO', 'atlas_api', 'atlas_api_snapshot_projects_for_org_id', cluster.name, snapshot_id);
-              }}).catch(err => {
-                context.functions.execute('log_message', 'ERROR', 'atlas_api', 'atlas_api_snapshot_projects_for_org_id', err, snapshot_id);
-              })
+              clusterSnapshotsCollection.updateOne({"_id": snapshot_id}, { $push : { 'clusters' : clusterDoc } }).then(result => {
+                const { matchedCount, modifiedCount } = result;
+                if( matchedCount && modifiedCount ) {
+                  context.functions.execute('log_message', 'INFO', 'atlas_api', 'atlas_api_snapshot_projects_for_org_id', cluster.name, snapshot_id);
+                }}).catch(err => {
+                  context.functions.execute('log_message', 'ERROR', 'atlas_api', 'atlas_api_snapshot_projects_for_org_id', err, snapshot_id);
+                })
             });
           });
         });
