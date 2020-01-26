00e724363c7a96c574d0da07d2bafb052b7b2e4a
JamesOsgood <james.osgood@gmail.com>
2020-01-26 22:09:25 +0000

    semicolons

--- functions/atlas_api_pause_cluster/source.js
+++ functions/atlas_api_pause_cluster/source.js
@@ -3,6 +3,6 @@ exports = function(projectId, clusterName)
 {
     url = "https://cloud.mongodb.com/api/atlas/v1.0/groups/"+projectId+"/clusters/" + clusterName;
     
-    body = { 'paused' : true }
+    body = { 'paused' : true };
     return context.functions.execute("atlas_api_patch", url, body, "atlas_api_pause_cluster");
 };
