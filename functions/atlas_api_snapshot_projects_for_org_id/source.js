d7ff0488b11f8f599c24b67fcdab1a7caf111948
JamesOsgood <james.osgood@gmail.com>
2020-01-27 21:11:53 +0000

    async me up

--- functions/atlas_api_snapshot_projects_for_org_id/source.js
+++ functions/atlas_api_snapshot_projects_for_org_id/source.js
@@ -41,8 +41,8 @@ function get_agg_pipeline(snapshot_id)
 async function insert_project_details(project_id, clusterSnapshotsDetails)
 {
     const resp = await context.functions.execute("atlas_api_get_clusters_for_project_id", project.id);
-    var clusters = resp.results;
-    clusters.forEach(cluster => {
+    for ( const cluster in resp.results ) 
+    {
       var clusterDoc = {
                   "snapshot_id" : snapshot_id,
                   "cluster_id": cluster.id,
@@ -55,21 +55,23 @@ async function insert_project_details(project_id, clusterSnapshotsDetails)
                   "configuration" : cluster};
 
         await clusterSnapshotsDetails.insertOne(clusterDoc);
-    });
+    };
 }
 
 async function insert_project_users(project_id, clusterSnapshotsDetails)
 {
-    const resp = context.functions.execute("atlas_api_get_users_for_project_id", project.id);
-    const users = resp.results;
-    users.forEach(user => {
-      user.roles.forEach(role => {
-        if ((role.groupId == project.id) && (role.roleName == "GROUP_OWNER") ) {
-          var userDoc = {"userId":user.id, "firstName": user.firstName, "lastName" : user.lastName, "emailAddress" : user.emailAddress };
-          await clusterSnapshotsDetails.updateMany({"snapshot_id" : snapshot_id, "project.id": project.id}, { $addToSet : {"users": userDoc}});
-        }
-    });
-  });
+  const resp = context.functions.execute("atlas_api_get_users_for_project_id", project.id);
+  for ( const user in resp.results )
+  {
+    for ( const role in user.roles)
+    {
+      if ((role.groupId == project.id) && (role.roleName == "GROUP_OWNER") ) 
+      {
+        var userDoc = { "userId" : user.id, "firstName" : user.firstName, "lastName" : user.lastName, "emailAddress" : user.emailAddress };
+        await clusterSnapshotsDetails.updateMany({"snapshot_id" : snapshot_id, "project.id": project.id}, { $addToSet : {"users": userDoc}});
+      }
+    }
+  }
 }
 
 exports = async function(org_id) 
@@ -82,11 +84,11 @@ exports = async function(org_id)
     const snapshot_id = snapshot_ts.toISOString();
 
     const resp_project = await context.functions.execute("atlas_api_get_projects_for_org_id", org_id);
-    var projects = resp.results;
-    projects.forEach(project => { 
+    for ( const project in resp.results)
+    { 
         await insert_project_details(project.id, clusterSnapshotsDetails);
         await insert_project_users_(project.id, clusterSnapshotsDetails);
-    });
+    };
 
     const pipeline = get_agg_pipeline(snapshot_id);
     await clusterSnapshotsDetails.aggregate(pipeline).toArray();
