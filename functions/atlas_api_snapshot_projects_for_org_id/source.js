2f36ae989ac0056ba2f902416e55e9a95df3f1f0
JamesOsgood <james.osgood@gmail.com>
2020-01-27 19:01:56 +0000

    get pipeline

--- functions/atlas_api_snapshot_projects_for_org_id/source.js
+++ functions/atlas_api_snapshot_projects_for_org_id/source.js
@@ -92,6 +92,7 @@ exports = function(org_id)
           });
         });
 
+        const pipeline = get_agg_pipeline(snapshot_id);
         return clusterSnapshotsDetails.aggregate(pipeline).then(doc => {
           context.functions.execute('log_message', 'INFO', 'atlas_api', 'atlas_api_snapshot_projects_for_org_id', 'Created Snapshot', snapshot_id);
           return snapshot_id;
