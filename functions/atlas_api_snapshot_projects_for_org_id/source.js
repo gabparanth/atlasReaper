b05756c6282d78a4969b63f3f49f9b0ecdc15ade
JamesOsgood <james.osgood@gmail.com>
2020-01-28 15:46:29 +0000

    Backing provider

--- functions/atlas_api_snapshot_projects_for_org_id/source.js
+++ functions/atlas_api_snapshot_projects_for_org_id/source.js
@@ -25,7 +25,7 @@ function get_agg_pipeline(snapshot_id)
                 'x', 
                 '$configuration.providerSettings.instanceSizeName', 
                 ' - ', 
-                '$configuration.providerSettings.providerName'
+                '$configuration.providerSettings.backingProviderName'
               ]
             }, 
             'details': {
