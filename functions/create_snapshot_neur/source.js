b15804df2ed2352953b27224e90f606385242297
Gabriel Paranthoen <gabrielparanthoen@Gabriels-MacBook-Pro.local>
2020-01-31 11:59:57 +0000

    bump

---
+++ functions/create_snapshot_neur/source.js
@@ -0,0 +1,5 @@
+exports = function() {
+
+  return context.functions.execute('atlas_api_snapshot_projects_for_org_id', '5aaf9d04df9db13ed52993e5');
+
+};
--- functions/updateWhitelistingPolicy/source.js
+++ functions/updateWhitelistingPolicy/source.js
@@ -1,6 +1,8 @@
 exports = function(id, whitelistingPolicy){
-const collection = context.services.get("MasterAtlas").db("atlas").collection("clusters");
-// console.log(arg);
-  var result = collection.updateOne({"_id": id}, { $set : {"whitelistingPolicy":whitelistingPolicy}});
+const collection = context.services.get("MasterAtlas").db("atlas").collection("active_clusters");
+
+console.log(id);
+
+  var result = collection.updateOne({"_id": BSON.ObjectId(id)}, { $set : {"whitelistingPolicy":whitelistingPolicy}});
 return result;
 };
