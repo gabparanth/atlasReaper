f64fc149b7dfdd88b730fc724adfb7ea956975cb
JamesOsgood <james.osgood@gmail.com>
2020-02-17 23:29:48 +0100

    More tasks

--- functions/trigger_execute_task/source.js
+++ functions/trigger_execute_task/source.js
@@ -21,6 +21,23 @@ exports = async function(changeEvent)
         await tasks.updateOne( {'_id' : task['_id']} , { '$set' : { 'status' : 'IN_PROGRESS' }});
         context.functions.execute('log_message', 'INFO', 'trigger', 'trigger_execute_task', `Paused cluster ${task.projectName}:${task.clusterName}`, task.snapshot_id);
     }
+    else if ( task_type == 'PAUSE_BI_CONNECTOR' )
+    {
+        try
+        {
+            await context.functions.execute('atlas_api_pause_bi_connector', task.details.project_id, task.cluster_name);
+        }
+        catch(err)
+        {
+            // Just log errors - can get an error if the cluster is paused in the interim
+            context.functions.execute('log_message', 'WARN', 'trigger', 'trigger_execute_task', `${task.projectName}:${task.clusterName} - ${err}`, task.snapshot_id);    
+        }
+
+        // Update task
+        const tasks = mongodb.db('atlas').collection('tasks');
+        await tasks.updateOne( {'_id' : task['_id']} , { '$set' : { 'status' : 'IN_PROGRESS' }});
+        context.functions.execute('log_message', 'INFO', 'trigger', 'trigger_execute_task', `Paused bi connector for cluster ${task.projectName}:${task.clusterName}`, task.snapshot_id);
+    }
     else
     {
         context.functions.execute('log_message', 'ERROR', 'trigger', 'trigger_execute_task', `Unknown task ${task_type}`, task.snapshot_id);
