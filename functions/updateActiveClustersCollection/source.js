8f73ed247cf41b338d5f0481f2c344f3f855c6e6
JamesOsgood <james.osgood@gmail.com>
2020-03-17 18:22:00 +0000

    Update

--- functions/updateActiveClustersCollection/source.js
+++ functions/updateActiveClustersCollection/source.js
@@ -1,5 +1,5 @@
-exports = async function() {
-
+exports = async function() 
+{
   const snapshot_id = await context.functions.execute('create_snapshot_neur');
   return context.functions.execute('process_snapshot', snapshot_id);
 };
